a1a75c67fd0b1bfc2e6047e74d7659c73690d40a
Kembo Franklyne <asininefatuity@gmail.com>
2025-03-14 02:05:41 +0300

    webpack folder setup done

--- content/__init__.py
+++ content/__init__.py
@@ -1,1 +1,4 @@
 from .package_json import PACKAGE_JSON_CONTENT
+from .webpack_common import WEBPACK_COMMON_CONTENT
+from .webpack_dev import WEBPACK_DEV_CONTENT
+from .webpack_prod import WEBPACK_PROD_CONTENT
--- set_up_frontend.py
+++ set_up_frontend.py
@@ -1,7 +1,7 @@
 import os 
 import subprocess 
 from constants import names
-from content import PACKAGE_JSON_CONTENT
+import content 
 
 class SetUpFrontend:
     PROJECT_ROOT = os.getcwd()
@@ -18,18 +18,28 @@ class SetUpFrontend:
         print("✅ Folder frontend created successfully")
     
     def _setup_webpack_config_files(self):
+        print("📦 Setting up webpack configuration files ...")
         os.makedirs(names.WEBPACK_DIR, exist_ok=True)
         os.chdir(names.WEBPACK_DIR)
+        with open(names.WEBPACK_COMMON_CONFIG_FILE, "w") as webpack_common_file:
+            webpack_common_file.write(content.WEBPACK_COMMON_CONTENT)
+        print("✅ webpack.common.js file created successfully")
+        with open(names.WEBPACK_DEV_CONFIG_FILE, "w") as webpack_dev_file:
+            webpack_dev_file.write(content.WEBPACK_DEV_CONTENT)
+        print("✅ webpack.dev.js file created successfully")
+        with open(names.WEBPACK_PROD_CONFIG_FILE, "w") as webpack_prod_file:
+            webpack_prod_file.write(content.WEBPACK_PROD_CONTENT)
+        print("✅ webpack.prod.js file created successfully")
+        print("✅ Webpack configuration files created successfully")
     
     def _setup_package_json_file(self):
         print("📦 Setting up package.json file and installing dependencies ...")
         with open(names.PACKAGE_JSON_FILE, "w") as package_json_file:
-            package_json_file.write(PACKAGE_JSON_CONTENT)
+            package_json_file.write(content.PACKAGE_JSON_CONTENT)
         print("✅ package.json file created successfully")
         print("📦 Installing dependencies ...")
         subprocess.run(["npm", "install"])
         print("✅ Dependencies installed successfully")
 
-
 frontend = SetUpFrontend()
-frontend._setup_package_json_file()
+frontend._setup_webpack_config_files()
