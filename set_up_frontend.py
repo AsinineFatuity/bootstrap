fc941ba6ca28f6ac15b3bbc8eb0c7ccf99c7ead8
Kembo Franklyne <asininefatuity@gmail.com>
2025-03-17 23:26:59 +0300

    fixed django urls file generator

--- set_up_frontend.py
+++ set_up_frontend.py
@@ -5,17 +5,41 @@ from utils import SetUpRootDir, SetUpFrontendDir, SetUpDjangoUrlsFile
 
 class SetUpFrontend:
 
-    def __init__(self):
+    def __init__(self, django_project_folder: str):
         self.project_root = os.getcwd()
+        self.django_project_folder = django_project_folder
 
     def set_up_frontend_project(self):
+        if not self._is_django_project_folder_valid():
+            return
+
         root_dir = SetUpRootDir(self.project_root)
         root_dir.set_up_root_dir()
         frontend_dir = SetUpFrontendDir(self.project_root)
         frontend_dir.set_up_frontend_dir()
-        django_urls_file = SetUpDjangoUrlsFile(self.project_root)
+        django_urls_file = SetUpDjangoUrlsFile(
+            self.project_root, self.django_project_folder
+        )
         django_urls_file.setup_django_urls_file()
 
+    def _is_django_project_folder_valid(self):
+        # check folder exists
+        if not os.path.exists(self.django_project_folder):
+            print(
+                f"❌ Django project folder {self.django_project_folder} does not exist in this directory"
+            )
+            return False
+        # check django folder is valid
+        unique_project_files = [names.DJANGO_ASGI_FILE, names.DJANGO_WSGI_FILE]
+        for root, dirs, files in os.walk(self.django_project_folder):
+            if any(file in files for file in unique_project_files):
+                return True
+        print(
+            "❌ Django project folder is not valid, please create a django project and try again"
+        )
+        return False
 
-frontend = SetUpFrontend()
+
+django_project_folder = input("Enter the name of your Django project folder name: ")
+frontend = SetUpFrontend(django_project_folder)
 frontend.set_up_frontend_project()
--- utils/setup_django_dir.py
+++ utils/setup_django_dir.py
@@ -4,23 +4,12 @@ from constants import names
 
 
 class SetUpDjangoUrlsFile:
-    def __init__(self, project_root: str):
+    def __init__(self, project_root: str, django_project_folder: str):
         self.PROJECT_ROOT = project_root
-        self.URLS_FILE = self._find_urls_file()
-
-    def _find_urls_file(self):
-        django_project_files = [names.DJANGO_ASGI_FILE, names.DJANGO_WSGI_FILE]
-        for root, dirs, files in os.walk(self.PROJECT_ROOT):
-            if any(file in files for file in django_project_files):
-                return os.path.join(root, names.DJANGO_URLS_FILE)
-        print(
-            "❌ Django urls.py file not found, please create django project and try again"
-        )
-        return None
+        self.URLS_FILE = os.path.join(django_project_folder, names.DJANGO_URLS_FILE)
 
     def setup_django_urls_file(self):
-        if self.URLS_FILE:
-            print("📦 Setting up Django urls.py file ...")
-            with open(self.URLS_FILE, "w") as urls_file:
-                urls_file.write(content.DJANGO_URLS_CONTENT)
-            print("✅ Django urls.py file created successfully")
+        print("📦 Setting up Django urls.py file ...")
+        with open(self.URLS_FILE, "w") as urls_file:
+            urls_file.write(content.DJANGO_URLS_CONTENT)
+        print("✅ Django urls.py file created successfully")
